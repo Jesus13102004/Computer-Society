95668bc8bb72dbc2ac6aec53d4f4512c5149e36a
Jesús Eduardo Arciniega Tlacomulco <jesuseduardo@MacBook-Air-de-Jesus-2.local>
2025-07-31 15:13:20 -0600

    Python Listas

---
+++ Python/Fundamentos_1/listas.py
@@ -0,0 +1,16 @@
+# Declaracion de listas
+
+list_1 = []  # Lista vacia
+list_2 = [1, 2, 3, 4] # Lista de numeros
+list_3 = ["Luis", "Pedro", "Jorge"] # Lista de strings
+list_4 = [23, "Hola", True, 3.9] # Lista con diferentes tipos de datos
+
+print(list_2[2])
+print(list_3[-1])
+
+list_1.append("Python") # Agrega "Python" a la lista 1
+list_2.insert(1, 7) # Agrega el valor 7 en el indice 1
+list_3.remove("Pedro") # Elimina "Pedro" de la lista
+list_4.pop() # Elimina el ultimo elemento de la lista
+
+
