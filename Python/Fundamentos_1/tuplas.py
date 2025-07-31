8b9cc2b3f581dc6d2c18a3e4f65ca290c607ac86
Jesús Eduardo Arciniega Tlacomulco <jesuseduardo@MacBook-Air-de-Jesus-2.local>
2025-07-31 15:33:16 -0600

    Python Tuplas

---
+++ Python/Fundamentos_1/tuplas.py
@@ -0,0 +1,8 @@
+# Declaracion de tuplas
+
+my_tupla = (1, "Hoal", False, 3.9) # Tupla con diferentes tipos de dato
+tupla = (5,) # Tupla de un solo elemento, se necesita poner "," para que sea tupla
+
+print(len(my_tupla))
+print(tupla[0])
+print(3.9 in my_tupla)
