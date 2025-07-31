5da34a75e076ef6c6369499deeef3206f8779bbd
Jesús Eduardo Arciniega Tlacomulco <jesuseduardo@MacBook-Air-de-Jesus-2.local>
2025-07-30 22:38:50 -0600

    Python variables

---
+++ Python/Fundamentos_1/variables.py
@@ -0,0 +1,28 @@
+my_string = "My string variable"  # Declarar variable
+print(my_string)  # Mostrarla en Terminal
+
+my_int = 10
+print(my_int)
+
+my_float = 4.9
+print(my_float)
+
+my_bool = True
+print(my_bool)
+
+
+# Concatenando variables 
+
+print(my_string, my_int, "Hola Mundo") # Para concatenar se separan las vareables con comas
+
+
+# Cambiar tipo de dato a una variable
+
+my_int_variable = 10  # Esto es una variable tipo int
+my_int_to_str = str(my_int_variable) # Se convierte la variable int a str
+print(type(my_int_to_str)) # esto muestra el tipo de dato que tiene la variable
+
+
+# Interaccion con el usuario
+age = input ("How old are you? ") # Se almacena en la variable age lo ingresado por el usuario
+print("Your age is:", age)
