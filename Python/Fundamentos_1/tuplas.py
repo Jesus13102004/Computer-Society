2d313234a9eff8cec9dfd4a17aa5d9279a3ca4b1
Jesús Eduardo Arciniega Tlacomulco <jesuseduardo@MacBook-Air-de-Jesus-2.local>
2025-07-31 15:33:43 -0600

    Python Tuplas 2.0

--- Python/Fundamentos_1/tuplas.py
+++ Python/Fundamentos_1/tuplas.py
@@ -3,6 +3,6 @@
 my_tupla = (1, "Hoal", False, 3.9) # Tupla con diferentes tipos de dato
 tupla = (5,) # Tupla de un solo elemento, se necesita poner "," para que sea tupla
 
-print(len(my_tupla))
-print(tupla[0])
-print(3.9 in my_tupla)
+print(len(my_tupla)) # Numero de elementos en la tupla
+print(tupla[0]) # Accede a un indice en especifico de la tupla
+print(3.9 in my_tupla) # Verifica si hay un elemento en la tupla
