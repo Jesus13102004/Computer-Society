b0fd68c0352a0bdc61b9a6e04405df2a13da0c75
Jesús Eduardo Arciniega Tlacomulco <jesuseduardo@MacBook-Air-de-Jesus-2.local>
2025-07-31 12:40:51 -0600

    Python operadores

---
+++ Python/Fundamentos_1/operadores.py
@@ -0,0 +1,48 @@
+# Operadpores Aritmeticos
+
+num1 = 10
+num2 = 2
+
+sum = num1 + num2 # Asignarle el resultado de la operacion entre dos variables a otra variable
+print(sum)
+
+res = 10 - 2  # Asignarle el resultado de la operacion entre dos numeros a una variable
+print(res)
+
+print(num1 * num2)  # Realizar una operacion de dos variables dentro de la funcion print()
+
+print(2 / 5)  # Realizar una operacion de dos numeros dentro de la funcion print()
+
+
+# Operadores de Asignacion
+num1 = num1 + 5
+print(num1)
+
+num1 += 5
+print(num1)
+
+
+# Operadores de Comparación
+
+print(num1 == num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 != num2)
+
+# Operadores Logicos
+
+print(num1>num2 and 5==10)
+print(num1>num2 or 5==10)
+
+
+# Operadores de Membresia
+
+print("a" in "hola")
+print("a" not in "hola")
+
+# Operadores de Identidad
+
+print(num1 is num2)
+print(num1 is not num2)
+
+
