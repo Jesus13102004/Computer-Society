d8d3930d12da673c1c7f645eed2abc615e758cdf
Jesús Eduardo Arciniega Tlacomulco <jesuseduardo@MacBook-Air-de-Jesus-2.local>
2025-07-31 18:41:11 -0600

    Python Condicionales

---
+++ Python/Fundamentos_1/condicionales.py
@@ -0,0 +1,18 @@
+# Ejemplo de condicional con operadores de comparacion
+x = 1
+if x > 5:
+    print("Mayor que 5")
+elif x == 3:
+    print("Igual a 3")
+else:
+    print("Ninguna de las anteriores")
+
+
+# Ejemplo de condicional con operadores logicos
+edad = 20
+tiene_credencial = True
+
+if edad >= 18 and tiene_credencial:
+    print("Puede entrar")
+else:
+    print("No puede entrar")
